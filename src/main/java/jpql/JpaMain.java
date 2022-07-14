fd60d78f2c6e7fb840f96436e583778cfcf29ade
hjyoon <kr-hjyoon@gmailcom>
2022-07-14 19:43:05 +0900

    완강

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,5 +1,8 @@
 package jpql;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -14,27 +17,58 @@ public class JpaMain {
 
         try{
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
+
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("member2");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m left join Team t on m.username = t.name" , Member.class)
-                    .getResultList();
+            //String query = "select m from Member m left join Team t on m.username = t.name";
+            String query = " update Member m set m.age=20 ";
 
-            System.out.println("result=" + result.size());
+            int updatedRows  = em.createQuery(query).executeUpdate();;
+            System.out.println("member1.getAge() = " + member1.getAge());
+            //System.out.println("updatedRows="+updatedRows);
 
-            for ( Member eachMember: result) {
+
+/*            for( Team team: result){
+                System.out.println("Team="+ team.getName()+ ", members = " + team.getMembers().size());
+            }*/
+
+            //Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            //System.out.println(gson.toJson(result));
+            //System.out.println(gson.toJson(result));
+/*
+            for(Object[] objects : result){
+                System.out.println("object=" + objects[0]);
+                System.out.println("object=" + objects[1]);
+                System.out.println("object=" + objects[2]);
+            }*/
+
+/*            for ( Member eachMember: result) {
                 System.out.println("member="+eachMember.toString()+",team="+eachMember.getTeam().toString());
-            }
+            }*/
 
             tx.commit();
 
